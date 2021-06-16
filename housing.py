376907a1be1f66f30a621ec6c3d5acc79e2521d8
Ayuba Tahiru <ayubamonnietahiru@gmail.com>
2021-06-16 23:23:56 +0200

    Day1

---
+++ housing.py
@@ -0,0 +1,55 @@
+import os
+import tarfile
+from six.moves import urllib
+import pandas as pd
+import matplotlib.pyplot as plt
+import numpy as np
+
+DOWNLOAD_ROOT = "https://raw.githubusercontent.com/ageron/handson-ml2/master/"
+HOUSING_PATH = os.path.join("datasets", "housing")
+HOUSING_URL = DOWNLOAD_ROOT + "datasets/housing/housing.tgz"
+
+
+def fetch_housing_data(housing_url=HOUSING_URL, housing_path=HOUSING_PATH):
+    if not os.path.isdir(housing_path):
+        os.makedirs(housing_path)
+    tgz_path = os.path.join(housing_path, "housing.tgz")
+    urllib.request.urlretrieve(housing_url, tgz_path)
+    housing_tgz = tarfile.open(tgz_path)
+    housing_tgz.extractall(path=housing_path)
+    housing_tgz.close()
+
+
+fetch_housing_data()
+
+
+def load_housing_data(housing_path=HOUSING_PATH):
+    csv_path = os.path.join(housing_path, "housing.csv")
+    return pd.read_csv(csv_path)
+
+
+housing = load_housing_data()
+first_5_data = housing.head()
+print(first_5_data,"\n")
+info_of_data = housing.info()
+print(info_of_data,"\n")
+category_in_data = housing["ocean_proximity"].value_counts()
+print(category_in_data,"\n")
+summary_numof_data = housing.describe()
+print(summary_numof_data,"\n")
+
+
+def split_train_test(data, test_ratio):
+    shuffled_indices = np.random.permutation(len(data))
+    test_set_size = int(len(data) * test_ratio)
+    test_indices = shuffled_indices[:test_set_size]
+    train_indices = shuffled_indices[test_set_size:]
+    return data.iloc[train_indices], data.iloc[test_indices]
+
+
+train_set, test_set = split_train_test(housing, 0.2)
+print(len(train_set))
+print(len(test_set))
+
+housing.hist(bins=50, figsize=(20, 15))
+plt.show()
