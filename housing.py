c4a1782a3ac7145f85a4330426aa260b37b4e1a1
Ayuba Tahiru <ayubamonnietahiru@gmail.com>
2021-06-21 00:04:02 +0200

    Day 2

--- housing.py
+++ housing.py
@@ -4,12 +4,17 @@ from six.moves import urllib
 import pandas as pd
 import matplotlib.pyplot as plt
 import numpy as np
+from zlib import crc32
+from sklearn.model_selection import train_test_split
+from sklearn.model_selection import StratifiedShuffleSplit
 
+# Source of data
 DOWNLOAD_ROOT = "https://raw.githubusercontent.com/ageron/handson-ml2/master/"
 HOUSING_PATH = os.path.join("datasets", "housing")
 HOUSING_URL = DOWNLOAD_ROOT + "datasets/housing/housing.tgz"
 
 
+# Function to fetching Housing Data
 def fetch_housing_data(housing_url=HOUSING_URL, housing_path=HOUSING_PATH):
     if not os.path.isdir(housing_path):
         os.makedirs(housing_path)
@@ -22,12 +27,12 @@ def fetch_housing_data(housing_url=HOUSING_URL, housing_path=HOUSING_PATH):
 
 fetch_housing_data()
 
-
+# Function to Load and concert the data file to pandas DataFrame
 def load_housing_data(housing_path=HOUSING_PATH):
     csv_path = os.path.join(housing_path, "housing.csv")
     return pd.read_csv(csv_path)
 
-
+# Glance through the Housing Data
 housing = load_housing_data()
 first_5_data = housing.head()
 print(first_5_data,"\n")
@@ -38,7 +43,8 @@ print(category_in_data,"\n")
 summary_numof_data = housing.describe()
 print(summary_numof_data,"\n")
 
-
+# Function to split the data for training, this is not good enough since the program will
+# generate different test set anytime it runs
 def split_train_test(data, test_ratio):
     shuffled_indices = np.random.permutation(len(data))
     test_set_size = int(len(data) * test_ratio)
@@ -47,9 +53,45 @@ def split_train_test(data, test_ratio):
     return data.iloc[train_indices], data.iloc[test_indices]
 
 
+# Divide the train and test set
 train_set, test_set = split_train_test(housing, 0.2)
-print(len(train_set))
+print(train_set)
 print(len(test_set))
+
+
+# def test_set_checker(identifier, test_ratio):
+#     return crc32(np.int64(identifier)) & 0xffffffff < test_ratio * 2**32
+#
+#
+# def split_train_test_by_id(data, test_ratio, id_column):
+#     ids = data[id_column]
+#     in_test_set = ids.apply(lambda id_: test_set_checker(id_, test_ratio))
+#     return data.loc[~in_test_set], data.loc[in_test_set]
+
+
+# housing_with_id = housing.reset_index()
+# train_set, test_set = split_train_test_by_id(housing_with_id, 0.2, "index")
+
+# housing_with_id = housing
+# housing_with_id["id"] = housing["longitude"] * 1000 + housing["latitude"]
+# train_set, test_set = split_train_test_by_id(housing_with_id, 0.2, "id")
+# print(train_set)
+
+
+train_set, test_set = train_test_split(housing, test_size=0.2, random_state=42)
+print(train_set)
+
+
+housing["income_cat"] = pd.cut(housing["median_income"], bins=[0.,1.5,3.0,4.5,6., np.inf], labels=[1,2,3,4,5])
+housing["income_cat"].hist()
+plt.show()
+
+split = StratifiedShuffleSplit(n_splits=1, test_size=0.2, random_state=42)
+for train_index, test_index in split.split(housing, housing["income_cat"]):
+    strat_train_set = housing.loc[train_index]
+    strat_test_set = housing.loc[test_index]
+    strat = strat_test_set["income_cat"].value_counts()/ len(strat_train_set)
+    print(strat)
 
 housing.hist(bins=50, figsize=(20, 15))
 plt.show()
