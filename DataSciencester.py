8f709dd660e146c6c2a216f455d77ff1f528095a
Ayuba Tahiru <ayubamonnietahiru@gmail.com>
2021-06-24 16:48:19 +0200

    day 3

--- DataSciencester.py
+++ DataSciencester.py
@@ -1,4 +1,6 @@
 from __future__ import division
+from collections import Counter
+from collections import defaultdict
 
 #---------------------------------------------------Key Connectors-----------------------------------------------------
 # Dump data
@@ -26,17 +28,112 @@ for i, j in friendships:
     users[i]["friends"].append(users[j])
     users[j]["friends"].append(users[i])
 
-# print(users)
-
-# Friends of each user has
+# Length of friends of each id
 def number_of_friends(user):
     return len(user["friends"])
 
 # Total connections
 total_connections = sum(number_of_friends(user) for user in users)
 
+print(total_connections)
+
 # Average connection
 num_users = len(users)
 avg_connections = total_connections / num_users
 print(avg_connections)
 
+# Most connected people
+num_friends_by_id = [(user["id"], number_of_friends(user)) for user in users]
+# print(num_friends_by_id)
+
+# Sort most connected people from largest to smallest
+sort_larg_to_small = sorted(num_friends_by_id, key= lambda num_friends:  num_friends[1], reverse=True)
+print(sort_larg_to_small)
+
+# Functions to produce a count mutual friends
+def not_the_same(user, other_user):
+    return user["id"] != other_user["id"]
+
+def not_friends(user, other_user):
+    return all(not_the_same(friend, other_user) for friend in user["friends"])
+
+def friends_of_friends_ids(user):
+    return Counter(foaf["id"]
+            for friend in user["friends"]
+            for foaf in friend["friends"]
+            if not_the_same(user, foaf)
+            and not_friends(user, foaf))
+
+print(friends_of_friends_ids(users[3]))
+
+
+# User interest
+interests = [
+    (0, "Hadoop"), (0, "Big Data"), (0, "HBase"), (0, "Java"),
+    (0, "Spark"), (0, "Storm"), (0, "Cassandra"),
+    (1, "NoSQL"), (1, "MongoDB"), (1, "Cassandra"), (1, "HBase"),
+    (1, "Postgres"), (2, "Python"), (2, "scikit-learn"), (2, "scipy"),
+    (2, "numpy"), (2, "statsmodels"), (2, "pandas"), (3, "R"), (3, "Python"),
+    (3, "statistics"), (3, "regression"), (3, "probability"),
+    (4, "machine learning"), (4, "regression"), (4, "decision trees"),
+    (4, "libsvm"), (5, "Python"), (5, "R"), (5, "Java"), (5, "C++"),
+    (5, "Haskell"), (5, "programming languages"), (6, "statistics"),
+    (6, "probability"), (6, "mathematics"), (6, "theory"),
+    (7, "machine learning"), (7, "scikit-learn"), (7, "Mahout"),
+    (7, "neural networks"), (8, "neural networks"), (8, "deep learning"),
+    (8, "Big Data"), (8, "artificial intelligence"), (9, "Hadoop"),
+    (9, "Java"), (9, "MapReduce"), (9, "Big Data")
+]
+
+# Function, Similar interest
+
+# keys are interests, values are lists of user_ids with that interest
+user_ids_by_interest = defaultdict(list)
+for user_id, interest in interests:
+    user_ids_by_interest[interest].append(user_id)
+
+# keys are user_ids, values are lists of interests for that user_id
+interests_by_user_id = defaultdict(list)
+for user_id, interest in interests:
+    interests_by_user_id[user_id].append(interest)
+
+
+def most_common_interests_with(user):
+    return Counter(interested_user_id
+                   for interest in interests_by_user_id[user["id"]]
+                   for interested_user_id in user_ids_by_interest[interest]
+                   if interested_user_id != user["id"])
+
+# Salaries and tenure of data scientist
+salaries_and_tenures = [
+    (83000, 8.7), (88000, 8.1),
+    (48000, 0.7), (76000, 6),
+    (69000, 6.5), (76000, 7.5),
+    (60000, 2.5), (83000, 10),
+    (48000, 1.9), (63000, 4.2)
+]
+
+# Function, Bucket of tenure
+salary_by_tenure_bucket = defaultdict(list)
+def tenure_bucket(tenure):
+    if tenure < 2:
+        return "less than two"
+    elif tenure < 5:
+        return "between two and five"
+    else:
+        return "more than five"
+
+
+# keys are tenure buckets, values are lists of salaries for that bucket
+
+
+for salary, tenure in salaries_and_tenures:
+    bucket = tenure_bucket(tenure)
+    salary_by_tenure_bucket[bucket].append(salary)
+
+# keys are tenure buckets, values are average salary for that bucket
+average_salary_by_bucket = {
+    tenure_bucket : sum(salaries) / len(salaries)
+    for tenure_bucket, salaries in salary_by_tenure_bucket.items()
+}
+print(average_salary_by_bucket)
