c37230f089b750b3e60eb2c3969c3fb7bae20f7d
Ayuba Tahiru <ayubamonnietahiru@gmail.com>
2021-07-13 03:38:55 +0200

    Day 11, K Mean

--- K_Mean.py
+++ K_Mean.py
@@ -2,8 +2,7 @@ from sklearn.cluster import KMeans
 import pandas as pd
 import matplotlib.pyplot as plt
 from sklearn.preprocessing import MinMaxScaler
-from sklearn.model_selection import train_test_split
-from sklearn.linear_model import LinearRegression
+
 
 
 age_income = {
