a73c556ce771641036bcc3f034c329dcf5e67a71
Ayuba Tahiru <Ayuba.Tahiru@dfki.de>
2021-10-04 11:08:27 +0200

    Data manipulations

--- housing.py
+++ housing.py
@@ -8,13 +8,13 @@ from zlib import crc32
 from sklearn.model_selection import train_test_split
 from sklearn.model_selection import StratifiedShuffleSplit
 
-# Source of data
+""" Source of data"""
 DOWNLOAD_ROOT = "https://raw.githubusercontent.com/ageron/handson-ml2/master/"
 HOUSING_PATH = os.path.join("datasets", "housing")
 HOUSING_URL = DOWNLOAD_ROOT + "datasets/housing/housing.tgz"
 
 
-# Function to fetching Housing Data
+""" Function to fetching Housing Data"""
 def fetch_housing_data(housing_url=HOUSING_URL, housing_path=HOUSING_PATH):
     if not os.path.isdir(housing_path):
         os.makedirs(housing_path)
@@ -27,71 +27,63 @@ def fetch_housing_data(housing_url=HOUSING_URL, housing_path=HOUSING_PATH):
 
 fetch_housing_data()
 
-# Function to Load and concert the data file to pandas DataFrame
+""" Function to Load and concert the data file to pandas DataFrame"""
 def load_housing_data(housing_path=HOUSING_PATH):
     csv_path = os.path.join(housing_path, "housing.csv")
     return pd.read_csv(csv_path)
 
-# Glance through the Housing Data
+
+"""Glance through the Housing Data"""
+
 housing = load_housing_data()
+# View data
+housing.hist(bins=50, figsize=(20, 15))
+# plt.show()
+
+# Check the first 5 of the dataset
 first_5_data = housing.head()
 print(first_5_data,"\n")
+
+# Check insight of the information on the dataset
 info_of_data = housing.info()
 print(info_of_data,"\n")
+
+# A deep view into ocean_proximity
 category_in_data = housing["ocean_proximity"].value_counts()
 print(category_in_data,"\n")
+
+# Check the statistics on the dataset
 summary_numof_data = housing.describe()
 print(summary_numof_data,"\n")
 
-# Function to split the data for training, this is not good enough since the program will
-# generate different test set anytime it runs
-def split_train_test(data, test_ratio):
-    shuffled_indices = np.random.permutation(len(data))
-    test_set_size = int(len(data) * test_ratio)
-    test_indices = shuffled_indices[:test_set_size]
-    train_indices = shuffled_indices[test_set_size:]
-    return data.iloc[train_indices], data.iloc[test_indices]
 
-
-# Divide the train and test set
-train_set, test_set = split_train_test(housing, 0.2)
-print(train_set)
-print(len(test_set))
-
-
-# def test_set_checker(identifier, test_ratio):
-#     return crc32(np.int64(identifier)) & 0xffffffff < test_ratio * 2**32
-#
-#
-# def split_train_test_by_id(data, test_ratio, id_column):
-#     ids = data[id_column]
-#     in_test_set = ids.apply(lambda id_: test_set_checker(id_, test_ratio))
-#     return data.loc[~in_test_set], data.loc[in_test_set]
-
-
-# housing_with_id = housing.reset_index()
-# train_set, test_set = split_train_test_by_id(housing_with_id, 0.2, "index")
-
-# housing_with_id = housing
-# housing_with_id["id"] = housing["longitude"] * 1000 + housing["latitude"]
-# train_set, test_set = split_train_test_by_id(housing_with_id, 0.2, "id")
-# print(train_set)
-
-
-train_set, test_set = train_test_split(housing, test_size=0.2, random_state=42)
-print(train_set)
-
-
+"""Understanding and knowing the best method of splitting of datasets into train and test"""
 housing["income_cat"] = pd.cut(housing["median_income"], bins=[0.,1.5,3.0,4.5,6., np.inf], labels=[1,2,3,4,5])
 housing["income_cat"].hist()
-plt.show()
+# plt.show()
+
+train_set, test_set = train_test_split(housing, test_size=0.2, random_state=42)
+
 
 split = StratifiedShuffleSplit(n_splits=1, test_size=0.2, random_state=42)
 for train_index, test_index in split.split(housing, housing["income_cat"]):
     strat_train_set = housing.loc[train_index]
     strat_test_set = housing.loc[test_index]
-    strat = strat_test_set["income_cat"].value_counts()/ len(strat_train_set)
-    print(strat)
+    strat = strat_test_set["income_cat"].value_counts()/ len(strat_test_set)
 
-housing.hist(bins=50, figsize=(20, 15))
-plt.show()
+
+"""Comparing bias in randomly split with stratified split with original income column"""
+# Original column of average income cat
+print(housing["income_cat"].value_counts()/ len(housing))
+# Random split average of income cat column
+print(test_set["income_cat"].value_counts()/ len(test_set))
+# stratified split average of income cat column
+print(strat)
+
+"""Drop income_cat in both strat_train_set and strat_test_set, since it was only used for analysis"""
+for drp in (strat_train_set, strat_test_set):
+    drp.drop("income_cat", axis=1, inplace=True)
+
+"""Create a copy of strat_train_set for manipulation purpose"""
+strat_train_set = strat_train_set.copy()
+print(strat_train_set)
